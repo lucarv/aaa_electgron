ec50c3bb76562a604c8a0a901039d9799a42d8ac
Tayo Carvalhal <lucarv@Tayos-MacBook-Pro.local>
2018-07-18 15:22:36 +0200

    simplified socket binding

--- sender.js
+++ sender.js
@@ -1,40 +1,27 @@
-let $ = require('jquery')
+let $ = require('jquery');
+let dgram = require('dgram');
 let messages = require('./lib/messages');
 let radius = require('./lib/radius');
-let dgram = require('dgram');
-let client = dgram.createSocket("udp4");
-let address = (process.env.AAA_HOST || '0.0.0.0');
-let aaa_port = (process.env.AAA_PORT || 1815);
-let udp_port = (process.env.UDP_PORT || 49002);
-client.bind({
-    address: address,
-    port: udp_port
-});
+let address = process.env.AAA_HOST || '192.168.1.11';
+let client = dgram.createSocket('udp4');
+client.bind(49001);
 
 $('#send-acct-msg').on('click', () => {
+	let ip = $('#ip').val();
+	let id = $('#id').val();
+	let msg_index = $('#msg_index').val();
 
-    let ip = $('#ip').val();
-    let id = $('#id').val();
-    let msg_index = $('#msg_index').val();
-
-    sendRadiusMessage(msg_index, ip, id);
-})
+	sendRadiusMessage(msg_index, ip, id);
+});
 
 function sendRadiusMessage(msg_index, ip, id) {
-    let message = messages[msg_index];
-    message.attributes[1] = ['Framed-IP-Address', ip];
-    message.attributes[2] = ['3GPP-IMEISV', id];
-    let encoded = radius.encode(message);
+	let message = messages[msg_index];
+	message.attributes[1] = ['Framed-IP-Address', ip];
+	message.attributes[2] = ['3GPP-IMEISV', id];
+	let encoded = radius.encode(message);
+	console.log(encoded.toString());
 
-    let sent_packets = {};
-    sent_packets[message.identifier] = {
-        raw_packet: encoded,
-        secret: message.secret
-    };
-    window.alert('Send RADIUS to: ' + address+':'+aaa_port);
-
-    client.send(encoded, 0, encoded.length, aaa_port, address, (err, bytes) => {
-        if (err) window.alert(err);
-        else window.alert('Accounting Message sent')
-    });
+	client.send(encoded, 1815, address, err => {
+		if (err) window.alert(err);
+	});
 }
